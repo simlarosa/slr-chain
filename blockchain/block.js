de849404813928865706e4e535c878d067497a9a
Simone La Rosa <SimoneMac@MacBook-Pro-di-Simone-2.local>
2018-10-28 23:36:02 +0100

    Added Proof of Work algorithm

--- blockchain/block.js
+++ blockchain/block.js
@@ -8,42 +8,61 @@ A block contains:
 We code a Block as a class */
 
 const SHA256 = require('crypto-js/sha256'); //we require sha256 function from library crypto-js
+const {DIFFICULTY, MINE_RATE} = require('../config');
 
 class Block {  //create a class model called Block
-  constructor(timestamp, lastHash, hash, data) { // We initizialize a class by the constructor and declare the attributes
+  constructor(timestamp, lastHash, hash, data, nonce, difficulty) { // We initizialize a class by the constructor and declare the attributes
     this.timestamp = timestamp; //We assign the attributes give to the constructor to the instance attributes of the class
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty || DIFFICULTY;
   }
 
   toString() { //we create a method needed for returing all the attributes of a given instance of the class
     return `Block -
-        Timestamp: ${this.timestamp}
-        Last Hash: ${this.lastHash.substring(0,10)}
-        Hash     : ${this.hash.substring(0,10)}
-        Data     : ${this.data}`; //substring method is used to limit the number of the Hash to display
+        Timestamp : ${this.timestamp}
+        Last Hash : ${this.lastHash.substring(0,10)}
+        Hash      : ${this.hash.substring(0,10)}
+        Nonce     : ${this.nonce}
+        Difficulty: ${this.difficulty}
+        Data      : ${this.data}`; //substring method is used to limit the number of the Hash to display
   }
 
   static genesis() { //By putting static identifier we can called the method without making a new instance of the Block
-    return new this('Genesis time','-------', 'f1r57-h4sh', 'This block is property of Simone La Rosa'); //This return a new instance of the class
+    return new this('Genesis time','-------', 'f1r57-h4sh', 'This block is property of Simone La Rosa', 0, DIFFICULTY); //This return a new instance of the class
   }
 
   static mineBlock(lastBlock, data) { //We create a static method that have the purpose of create new block and needs as attribute lastBlock and data
-    const timestamp = Date.now(); //As timestamp we use js library calle Date and his method now that return in milliseconds the time pass from 1 January 1970
+    let hash, timestamp;
     const lastHash = lastBlock.hash; //Take the hash of the lastBlock
-    const hash = Block.hash(timestamp,lastHash, data); //we create the hash thanks to the hash method in the class
+    let { difficulty } = lastBlock;
+    let nonce = 0;
 
-    return new this(timestamp, lastHash, hash, data); //this create a new instance of the block
+    do{
+      nonce++;
+      timestamp = Date.now();  //As timestamp we use js library calle Date and his method now that return in milliseconds the time pass from 1 January 1970
+      difficulty = Block.adjustDifficulty(lastBlock, timestamp);
+      hash = Block.hash(timestamp,lastHash, data, nonce, difficulty); //we create the hash thanks to the hash method in the class
+    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+
+    return new this(timestamp, lastHash, hash, data, nonce, difficulty); //this create a new instance of the block
   }
 
-  static hash(timestamp, lastHash, data) { //we create an hash function that combine timestamp and lastHash and data for create the Hash
-    return SHA256(`${timestamp}${lastHash}${data}`).toString(); //calling SHA256 function of the crypto-js library
+  static hash(timestamp, lastHash, data, nonce, difficulty) { //we create an hash function that combine timestamp and lastHash and data for create the Hash
+    return SHA256(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString(); //calling SHA256 function of the crypto-js library
   }
 
   static blockHash(block){ //receive a block and calculate is own hash base on the data inside it
-    const {timestamp, lastHash, data} = block; //put the attribute equal to the own of the block
-    return Block.hash(timestamp, lastHash, data); //The hash function
+    const {timestamp, lastHash, data, nonce, difficulty} = block; //put the attribute equal to the own of the block
+    return Block.hash(timestamp, lastHash, data, nonce,difficulty); //The hash function
+  }
+
+  static adjustDifficulty(lastBlock, currentTime) {
+    let { difficulty } = lastBlock;
+    difficulty = lastBlock.timestamp + MINE_RATE > currentTime ? difficulty + 1 : difficulty - 1;
+    return difficulty;
   }
 }
 
--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,5 +1,5 @@
 const Block = require('./block');
-
+const {DIFFICULTY} = require('../config');
 //We will use jest to run the test and this is the way to write a test:
 
 describe('Block', () =>{ //describe is the method the create a Test and in this case we called Block
@@ -17,5 +17,18 @@ let data, lastBlock, block; //we declare the variable to be use in each function
 
   it('sets the `lastHash` to match the hash of the last block', () => {
     expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+
+  it('generates a hash that matches the difficulty', () => {
+    expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+    console.log(block.toString());
+  });
+
+  it('lowes the difficulty for slowly mined blocks', ()=> {
+   expect(Block.adjustDifficulty(block,block.timestamp+360000)).toEqual(block.difficulty-1);
+  });
+
+  it('raises the difficulty for quickly mined blocks', ()=> {
+   expect(Block.adjustDifficulty(block,block.timestamp+1)).toEqual(block.difficulty+1);
   });
 });
