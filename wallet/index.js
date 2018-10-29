1d2f9f0121bc000d23774b0a05b18278436687e5
Simone La Rosa <SimoneMac@MBP-di-Simone-2.homenet.telecomitalia.it>
2018-10-29 15:15:37 +0100

    Add Transactions Pool model

--- wallet/index.js
+++ wallet/index.js
@@ -1,6 +1,6 @@
 const ChainUtil = require('../chain-util'); //require the chain util class
 const { INITIAL_BALANCE } = require('../config'); //require the initial balance const
-
+const Transaction = require('./transaction');
 
 class Wallet { //create a Wallet object
   constructor(){ //the constructor define the balance, the Key pair and the public key
@@ -17,6 +17,23 @@ class Wallet { //create a Wallet object
 
   sign(dataHash) { //the sign method that take an hash as attributes
     return this.keyPair.sign(dataHash); //ritorna il sign method of the library ec
+  }
+
+  createTransaction(recipient, amount, transactionPool) {
+    if(amount > this.balance){
+      console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`);
+      return;
+    }
+    let transaction = transactionPool.existingTransaction(this.publicKey);
+
+    if(transaction){
+      transaction.update(this, recipient, amount);
+    } else {
+      transaction = Transaction.newTransaction(this, recipient, amount);
+      transactionPool.updateOrAddTransaction(transaction);
+    }
+
+    return transaction;
   }
 }
 
