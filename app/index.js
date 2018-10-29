9d39582dfefe2b9d3d6e379cecee3018b0b78d5d
Simone La Rosa <SimoneMac@MBP-di-Simone-2.homenet.telecomitalia.it>
2018-10-29 15:28:59 +0100

    Add in the app the GET and POST for transaction

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@ const express = require('express'); //richediamo il modulo express, body parser 
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require ('../wallet'); //include the wallet class
+const TransactionPool = require('../wallet/transaction-pool'); //include transaction poll class
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001; //definiamo la porta HTTP che può essere o definita dal terminale o è la 3001
 
 const app = express(); //dichiariamo l'oggetto app che è express
 const bc = new Blockchain(); //dichiariamo un'istanza della Blockchain
+const wallet = new Wallet(); //declare an instance of the wallet
+const tp = new TransactionPool(); //declare an instance of the TransactionPool
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json()); //utilizziamo il bodyParser per ricevere risposte in POST in formato JSON
@@ -22,6 +26,16 @@ app.post('/mine', (req, res) => { //dichiariamo un API post
   p2pServer.syncChains();
 
   res.redirect('/blocks'); //rindirizziamo alla chiamata GET
+});
+
+app.get('/transactions', (req, res)=> { //create a get API for see all the transactions in a pool
+  res.json(tp.transactions); //give as response a json form of the transactions in the transaction pool
+});
+
+app.post('/transact', (req, res) => { //create a post API to make a transaction
+  const { recipient, amount } = req.body; //declare the request body and take the data
+  const transaction = wallet.createTransaction(recipient, amount, tp); //create the transaction from the wallet
+  res.redirect('/transactions'); //redirect to see the transaction
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`)); //Asscoltiamo nell pota 3001
